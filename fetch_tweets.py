54f897ce8d140a8204d80eb23bba08d196a76638
Marcel Guzik <mguzik555@gmail.com>
2021-10-07 18:18:25 +0200

    save the tweets with the date in the filename

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -3,10 +3,20 @@
 import os
 import requests
 import json
+import sys
+import datetime
+
 
 f = open('bearer_token.txt')
 BEARER_TOKEN = f.read().strip('\n')
 f.close()
+
+time = datetime.datetime.now().isoformat(
+    sep='_', timespec='seconds').replace(':', '-')
+result_filename = f'tweets/tweets_{time}.json'
+
+if len(sys.argv) == 2:
+    result_filename = sys.argv[1]
 
 names = [
     'AgaScigaj',
@@ -21,18 +31,21 @@ names = [
 ]
 names = ','.join(names)
 
+MAX_RESULTS = 10
+
+print(f'Fetching tweets of: {names}')
+
 res = requests.get(f'https://api.twitter.com/2/users/by?usernames={ names }', headers={
                    'Authorization': f'Bearer {BEARER_TOKEN}'})
-
 ids = list(map(lambda user: user['id'], json.loads(res.text)['data']))
-print(ids)
-
 
 tweets = list(map(lambda id: json.loads(
-    requests.get(f'https://api.twitter.com/2/users/{id}/tweets?tweet.fields=public_metrics',
+    requests.get(f'https://api.twitter.com/2/users/{id}/tweets?tweet.fields=public_metrics,created_at&expansions=author_id&max_results={MAX_RESULTS}&user.fields=name',
                  headers={'Authorization': f'Bearer {BEARER_TOKEN}'}).text), ids))
 
 print(tweets)
-f = open('tweets.json', 'w')
+f = open(result_filename, 'w')
 json.dump(tweets, f, indent=4, ensure_ascii=False)
 f.close()
+
+print(f'Results written to: {result_filename}')
